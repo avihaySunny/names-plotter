01d189fcf583ee0c76658373cfd7eddc3361eada
Avihay Asraf <aasraf@C02YV3D6LVCJ.corp.proofpoint.com>
2022-07-23 10:55:55 +0300

    normalization

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import {JEWISH_WOMEN} from "./datasets/jwomen";
 import { MMEN } from "./datasets/mmen";
 import {M_WOMEN} from "./datasets/mwomen";
 import AsyncSelect from "react-select/async";
+import {CMEN} from "./datasets/cmen";
+import {CWOMEN} from "./datasets/cwomen";
+import {DMEN} from "./datasets/dmen";
+import {DWOMEN} from "./datasets/dwomen";
+import {OTHERS} from "./datasets/other";
+import {FOTHERS} from "./datasets/fml-others";
 
 ChartJS.register(...registerables);
 
@@ -23,47 +29,108 @@ const getRandombckColor = () => {
   return `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`
 }
 
+const parseYearValue = yearValue => {
+  if (yearValue === ".") return 0;
+  if(yearValue === "..") return Math.floor(Math.random() * 4);
+  return parseInt(yearValue?.replaceAll(',', ''));
+}
+
+const getNormalizeValueByYear = (dataset, year) => {
+  const values = dataset.map(item => parseYearValue(item[year]));
+  return values.reduce((a, b) => a + b, 0);
+}
+const createNormValues = dataset => {
+  const yearsList = range(1948, 2021);
+  const normValues = {};
+  yearsList.forEach(year => {
+    normValues[year] = getNormalizeValueByYear(dataset, year);
+  })
+  return normValues;
+}
+let sortValue = 1;
 const selectOptions = [
   {
     label: 'יהודים',
-    value: 1,
+    value: sortValue += 1,
     dataset: JEWISH_MEM,
+    normValues: createNormValues(JEWISH_MEM),
   },
   {
     label: 'יהודיות',
-    value: 2,
+    value: sortValue += 1,
     dataset: JEWISH_WOMEN,
+    normValues: createNormValues(JEWISH_WOMEN),
   },
   {
     label: 'מוסלמים',
-    value: 3,
+    value: sortValue += 1,
     dataset: MMEN,
+    normValues: createNormValues(MMEN),
   },
   {
     label: 'מוסלמיות',
-    value: 4,
+    value: sortValue += 1,
     dataset: M_WOMEN,
+    normValues: createNormValues(M_WOMEN),
   },
-
+  {
+    label: 'נוצרים',
+    value: sortValue += 1,
+    dataset: CMEN,
+    normValues: createNormValues(CMEN),
+  },
+  {
+    label: 'נוצריות',
+    value: sortValue += 1,
+    dataset: CWOMEN,
+    normValues: createNormValues(CWOMEN),
+  },
+  {
+    label: 'דרוזים',
+    value: sortValue += 1,
+    dataset: DMEN,
+    normValues: createNormValues(DMEN),
+  },
+  {
+    label: 'דרוזיות',
+    value: sortValue += 1,
+    dataset: DWOMEN,
+    normValues: createNormValues(DWOMEN),
+  },
+  {
+    label: 'אחרים',
+    value: sortValue += 1,
+    dataset: OTHERS,
+    normValues: createNormValues(OTHERS),
+  },
+  {
+    label: 'אחרות',
+    value: sortValue += 1,
+    dataset: FOTHERS,
+    normValues: createNormValues(FOTHERS),
+  },
 ]
-const parseYearValue = yearValue => {
-  if (yearValue.includes(',')) {
-    const value = yearValue.replaceAll(',', '');
-    return parseInt(value);
-  }
-  if (yearValue === ".") return 0;
-  if(yearValue === "..") return Math.floor(Math.random() * 4);
-  return parseInt(yearValue);
-}
+
 
 const labels = range(1948, 2021);
 
-const createDataFromRow = row => {
+const createDataFromRow = (datasheet, selectedName) => {
+  const row = datasheet.dataset?.find(item => item.name === selectedName.label);
   const y = labels.map(year => parseYearValue(row[year]));
+  const normalized = labels.map(year => {
+    const yearTotal = datasheet.normValues[year];
+    if (yearTotal) {
+      const yearValue = parseYearValue(row[year]);
+      return 100*(yearValue/yearTotal);
+    }
+    return 0;
+  });
+
   return ({
     y,
     name: row.name,
     total: row.total,
+    normalized,
   });
 }
 
@@ -72,18 +139,17 @@ function App() {
   const randomDatasheet = getRandomDatasheet();
   const [datasheet, setDatasheet] = useState(randomDatasheet);
   const [selectedName, setSelectedName] = useState({});
-
   const [defaultOptions, setDefOptions] = useState([]);
+  const [shouldUseNorm, setShouldUseNorm] = useState(false);
 
   const addDataSet = () => {
-    debugger
     if (selectedName.label) {
-      const newDataset = createDataFromRow(datasheet.dataset.find(item => item.name === selectedName.label))
+      const newDataset = createDataFromRow(datasheet, selectedName);
       const color = getRandombckColor();
       setDatasets(prev => [
         ...prev, {
           label: newDataset.name,
-          data: newDataset.y,
+          data: shouldUseNorm ? newDataset.normalized : newDataset.y,
           borderColor: color,
           backgroundColor: color,
         }
@@ -125,9 +191,15 @@ function App() {
 
   // eslint-disable-next-line
   useEffect(addDataSet, [selectedName]);
+
+  const onCheckboxChange = e => {
+    setShouldUseNorm(e.target.checked);
+    setDatasets([]);
+  }
   return (
     <div className="main-view">
-      <div className="plot-and-search">
+      <div className="plot-wrapper">
+        <div className="plot-and-search">
         <div className="search-input">
           <div title="נקה את הרשימה" className="clear-icon" onClick={() => setDatasets([])}>
             <DeleteIcon />
@@ -140,15 +212,22 @@ function App() {
           className="name-select"
           isRtl
         />
-          <ReactSelect
+        <ReactSelect
           options={selectOptions}
           value={datasheet}
           onChange={setDatasheet}
           isRtl
           className="data-select"
         />
+        <div className="toggle-wrapper">
+          <input
+            type="checkbox"
+            value={shouldUseNorm}
+            onChange={onCheckboxChange}
+          />
+          <span> נרמל </span>
+        </div>
       </div>
-      <div className="plot-wrapper">
         <Line
           className="plot"
           datasetIdKey='id'
